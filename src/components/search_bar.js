9346f8ed3b5dac36f2a463d2248dfb252a771b5d
Ruston Reformado <rustonreformado@gmail.com>
2018-02-15 00:50:47 -0700

    complete youtube app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,14 +9,23 @@ class SearchBar extends Component {
         };
     }
 
+    // setting state seems unnecessary here
+
     render(){
         return (
-            <div>
+            <div className='search-bar'>
                 <input
                     value={this.state.term} 
-                    onChange={(event) => this.setState({ term: event.target.value })} />
+                    onChange={(event) => this.onInputChange(event.target.value)} />
             </div>
         )
+    }
+
+    onInputChange(term) {
+        this.setState({
+            term: term
+        });
+        this.props.onSearchTermChange(term);
     }
 }
 
